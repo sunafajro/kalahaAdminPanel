63a199a96f3ee31ac30285ea30bc61f4dc9cd217
Evgeny Belkin <sunafajro@gmail.com>
2017-11-18 19:08:45 +0300

    добавлены новые элементы и названия

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
-import { logout } from '../../modules/actions/auth';
+import { logout } from "../../modules/actions/auth";
 
 class Navigation extends React.Component {
   handleLogout = e => {
@@ -33,6 +33,21 @@ class Navigation extends React.Component {
             id="navbarSupportedContent"
           >
             <ul className="navbar-nav">
+              <li className={ props.location.pathname === "/backend/game" ? "nav-item active" : "nav-item" }>
+                <Link to={"/backend/game"} className="nav-link">
+                  {props.labels.gameLinkTitle[props.language]}
+                </Link>
+              </li>
+              <li className={ props.location.pathname === "/backend/categories" ? "nav-item active" : "nav-item" }>
+                <Link to={"/backend/categories"} className="nav-link">
+                  {props.labels.categoriesLinkTitle[props.language]}
+                </Link>
+              </li>
+              <li className={ props.location.pathname === "/backend/words" ? "nav-item active" : "nav-item" }>
+                <Link to={"/backend/words"} className="nav-link">
+                  {props.labels.wordsLinkTitle[props.language]}
+                </Link>
+              </li>
               <li
                 className={
                   props.location.pathname === "/backend/login"
@@ -46,11 +61,11 @@ class Navigation extends React.Component {
                     className="nav-link"
                     onClick={this.handleLogout}
                   >
-                    Выход ({props.user.username})
+                    {props.labels.loginLinkTitle[props.language]} ({props.user.username})
                   </a>
                 ) : (
                   <Link to={"/backend/login"} className="nav-link">
-                    Вход
+                    {props.labels.logoutLinkTitle[props.language]}
                   </Link>
                 )}
               </li>
@@ -64,7 +79,9 @@ class Navigation extends React.Component {
 
 const mapStateToProps = state => ({
   loggedIn: state.app.loggedIn,
-  user: state.app.user
+  user: state.app.user,
+  language: state.app.language,
+  labels: state.app.labels.navigation
 });
 
 const mapDispatchToProps = dispatch =>
--- src/modules/constants/labels.js
+++ src/modules/constants/labels.js
@@ -1,4 +1,26 @@
 export const LABELS = {
+  navigation: {
+    gameLinkTitle: {
+      cv: '',
+      ru: 'Игра'
+    },
+    categoriesLinkTitle: {
+      cv: '',
+      ru: 'Категории'
+    },
+    wordsLinkTitle: {
+      cv: '',
+      ru: 'Слова'
+    },
+    loginLinkTitle: {
+      cv: '',
+      ru: 'Вход'
+    },
+    logoutLinkTitle: {
+      cv: '',
+      ru: 'Выход'
+    }
+  },
   login: {
     loginPageTitle: {
         cv: "",
