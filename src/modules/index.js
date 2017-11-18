6678037aaa52e6bc21276343f9cfadf8d1e172c6
Evgeny Belkin <sunafajro@gmail.com>
2017-11-19 00:01:17 +0300

    подключен модуль для игры

--- src/modules/index.js
+++ src/modules/index.js
@@ -4,11 +4,13 @@ import app from './reducers/app';
 import auth from './reducers/auth';
 import categories from './reducers/categories';
 import words from './reducers/words';
+import quiz from './reducers/quiz';
 
 export default combineReducers({
   routing: routerReducer,
   app,
   auth,
   categories,
-  words
+  words,
+  quiz
 })
