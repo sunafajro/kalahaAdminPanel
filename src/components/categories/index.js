0d1668e0fceebb7a335a311b43726a6dd9361e3e
Evgeny Belkin <sunafajro@gmail.com>
2017-11-18 22:45:12 +0300

    временные пустые компоненты

---
+++ src/components/categories/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Categories extends React.Component {
+  render() {
+    return (
+      <div>It Works</div>
+    );
+  }
+}
+
+export default Categories;
---
+++ src/components/words/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Words extends React.Component {
+  render() {
+    return (
+      <div>It Works</div>
+    );
+  }
+}
+
+export default Words;
