f1b4401edf4c80afad9bd8834b2cbd4e9194670b
Evgeny Belkin <sunafajro@gmail.com>
2017-11-18 18:30:51 +0300

    названия элементов приложения (пока только для формы входа)

---
+++ src/modules/constants/labels.js
@@ -0,0 +1,29 @@
+export const LABELS = {
+  login: {
+    loginPageTitle: {
+        cv: "",
+        ru: "Форма входа"
+      },
+      usernameLabel: {
+        cv: "",
+        ru: "Логин"
+      },
+      usernamePlaceholder: {
+        cv: "",
+        ru: "Введите логин"
+      },
+      passwordLabel: {
+        cv: "",
+        ru: "Пароль"
+      },
+      passwordPlaceholder: {
+        cv: "",
+        ru: "Введите пароль"
+      },
+      submitBtnLabel: {
+        cv: "",
+        ru: "Войти"
+      }
+  }
+};
+
