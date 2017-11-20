f3d87666aa2e8df82b75968135e1bd7522e7c9ee
Evgeny Belkin <sunafajro@gmail.com>
2017-11-20 23:05:43 +0300

    изменено форматирование

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-    entry: [
-        './src/index.js'
-    ],
-    output: {
-        path: __dirname + '/dist',
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
-    devtool: 'cheap-module-source-map',
-    module: {
-        loaders: [{
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loaders: ['babel-loader'],
-        }]
-        },
-        resolve: {
-            extensions: ['*', '.js', '.jsx']
-        },
-    };
+  entry: ['./src/index.js'],
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devtool: 'cheap-module-source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: ['babel-loader'],
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js'],
+  },
+};
