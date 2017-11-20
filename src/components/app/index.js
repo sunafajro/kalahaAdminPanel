42cbce04a746a1c121b8a9cac1464b2892aa0b8b
Evgeny Belkin <sunafajro@gmail.com>
2017-11-20 23:07:21 +0300

    изменено форматирование, добавлена проверка propTypes

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,40 +1,64 @@
-import React from 'react';
-import { Route, Redirect, Switch, withRouter } from 'react-router-dom';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { getState } from '../../modules/actions/app';
-import Navigation from '../navigation';
-import Game from '../game';
-import Auth from '../auth';
-import Categories from '../categories';
-import Words from '../words';
+import React from "react";
+import { Route, Redirect, Switch, withRouter } from "react-router-dom";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { bool, func } from "prop-types";
+
+import { getState } from "../../modules/actions/app";
+import Navigation from "../navigation";
+import Game from "../game";
+import Auth from "../auth";
+import Categories from "../categories";
+import Words from "../words";
 
 class App extends React.Component {
-  componentDidMount () {
+  static propTypes = {
+    appLoaded: bool.isRequired,
+    fetching: bool.isRequired,
+    loggedIn: bool.isRequired,
+    getState: func.isRequired
+  };
+
+  componentDidMount() {
     this.props.getState();
   }
 
-  render () {
-    let props = this.props;    
+  render() {
+    let props = this.props;
     return (
       <div>
-        { !props.appLoaded ? 
-          <div className="alert alert-warning">Загружаем приложение...</div> : 
+        {!props.appLoaded ? (
+          <div className="alert alert-warning">Загружаем приложение...</div>
+        ) : (
           <div>
-            { (!props.loggedIn && (props.location.pathname === '/backend/categories' || props.location.pathname === '/backend/words')) ?
-              <Redirect to='/backend/login' /> : '' }
-            { !props.fetching ?
+            {!props.loggedIn &&
+            (props.location.pathname === "/backend/categories" ||
+              props.location.pathname === "/backend/words") ? (
+              <Redirect to="/backend/login" />
+            ) : (
+              ""
+            )}
+            {!props.fetching ? (
               <div>
-                <Navigation location={ this.props.location } />
+                <Navigation location={this.props.location} />
                 <Switch>
-                  <Route exact path='/backend/login' component={ Auth }/>
-                  <Route exact path='/backend/categories' component={ Categories }/>
-                  <Route exact path='/backend/words' component={ Words }/>
-                  <Route path='/' component={ Game }/>
+                  <Route exact path="/backend/login" component={Auth} />
+                  <Route
+                    exact
+                    path="/backend/categories"
+                    component={Categories}
+                  />
+                  <Route exact path="/backend/words" component={Words} />
+                  <Route path="/" component={Game} />
                 </Switch>
-              </div> : <div className="alert alert-warning">Загружаем данные приложения...</div> }
+              </div>
+            ) : (
+              <div className="alert alert-warning">
+                Загружаем данные приложения...
+              </div>
+            )}
           </div>
-        }
+        )}
       </div>
     );
   }
@@ -46,8 +70,12 @@ const mapStateToProps = state => ({
   loggedIn: state.app.loggedIn
 });
 
-const mapDispatchToProps = dispatch => bindActionCreators({
-  getState
-}, dispatch);
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      getState
+    },
+    dispatch
+  );
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
