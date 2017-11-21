88d9f1994c2ac36603929dd28b216c02517288a0
Evgeny Belkin <sunafajro@gmail.com>
2017-11-21 20:08:34 +0300

    переписываем компонент Audio

--- src/components/game/audio.js
+++ src/components/game/audio.js
@@ -1,34 +1,30 @@
 import React from "react";
 
-class Audio extends React.Component {
-  render() {
-    return (
-      <div>
-        <audio id="teres">
-          <source
-            src="/sites/default/files/forchildren/teres.ogg"
-            type="audio/ogg"
-          />
-          <source
-            src="/sites/default/files/forchildren/teres.mp3"
-            type="audio/mp3"
-          />
-          Ваш браузер не поддерживает тег audio!
-        </audio>
-        <audio id="teres_mar">
-          <source
-            src="/sites/default/files/forchildren/teres_mar.ogg"
-            type="audio/ogg"
-          />
-          <source
-            src="/sites/default/files/forchildren/teres_mar.mp3"
-            type="audio/mp3"
-          />
-          Ваш браузер не поддерживает тег audio!
-        </audio>
-      </div>
-    );
-  }
+export default function Audio() {
+  return (
+    <div>
+      <audio id="teres">
+        <source
+          src="/files/audio/teres.ogg"
+          type="audio/ogg"
+        />
+        <source
+          src="/files/audio/teres.mp3"
+          type="audio/mp3"
+        />
+        Ваш браузер не поддерживает тег audio!
+      </audio>
+      <audio id="teres_mar">
+        <source
+          src="/files/audio/teres_mar.ogg"
+          type="audio/ogg"
+        />
+        <source
+          src="/files/audio/teres_mar.mp3"
+          type="audio/mp3"
+        />
+        Ваш браузер не поддерживает тег audio!
+      </audio>
+    </div>
+  );
 }
-
-export default Audio;
