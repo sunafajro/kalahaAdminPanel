40ee2cf187c3a67366ea3d793b270d868ca05ad5
Evgeny Belkin <sunafajro@gmail.com>
2017-11-22 21:19:03 +0300

    константы вынесены в отдельный файл

---
+++ src/components/game/constants.js
@@ -0,0 +1,9 @@
+export const CLASSLIST_DEFAULT = "card bg-light text-center";
+export const CLASSLIST_CORRECT = "card text-white bg-success text-center";
+export const CLASSLIST_INCORRECT = "card text-white bg-danger text-center";
+export const CLASSLIST = {
+  1: CLASSLIST_DEFAULT,
+  2: CLASSLIST_DEFAULT,
+  3: CLASSLIST_DEFAULT,
+  4: CLASSLIST_DEFAULT
+};
