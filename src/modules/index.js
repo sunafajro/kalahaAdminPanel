d74d23242c225570ba4c2de6395bf01c6d55bf8e
Evgeny Belkin <sunafajro@gmail.com>
2017-11-22 21:18:22 +0300

    убран quiz из редьюесеров

--- src/modules/index.js
+++ src/modules/index.js
@@ -4,13 +4,11 @@ import app from './reducers/app';
 import auth from './reducers/auth';
 import categories from './reducers/categories';
 import words from './reducers/words';
-import quiz from './reducers/quiz';
 
 export default combineReducers({
   routing: routerReducer,
   app,
   auth,
   categories,
-  words,
-  quiz
+  words
 })
