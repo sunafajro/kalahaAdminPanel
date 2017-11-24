f0f2066b1e62d48159eb0ec51daf3f5f54239b62
Evgeny Belkin <sunafajro@gmail.com>
2017-11-25 00:38:15 +0300

    добавлены переводы

--- src/translations/words.js
+++ src/translations/words.js
@@ -2,5 +2,25 @@ export const Translations = {
   appDataLoadingProccess: {
     cv: "Загружаем данные приложения...",
     ru: "Загружаем данные приложения..."
+  },
+  newWordFieldTitleCV: {
+    cv: "Сăмах (чӑвашла)",
+    ru: "Слово (чувашский)"
+  },
+  newWordFieldTitleRU: {
+    cv: "Сăмах (вырӑсла)",
+    ru: "Слово (русский)"
+  },
+  newWordFieldTitleImage: {
+    cv: "Ӳкерчӗк",
+    ru: "Изображение"
+  },
+  newWordFieldTitleAudio: {
+    cv: "Аудио",
+    ru: "Аудио"
+  },
+  filtersBlockTitle: {
+    cv: "Фильтрация",
+    ru: "Фильтрация"
   }
 };
